c899c5745ad3243644d93b2f01f64a71676fd7d9
steve-ux <exe.universal@gmail.com>
2023-02-16 23:36:26 -0300

    se inició el proyecto y se creó su estructura

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+    <div className="home">
+    <p><strong>Navbar</strong> con logo en la parte superior izquierda y menú a la derecha (Home, Quienes Somos, Contacto).</p>
+    <p><strong>Foto de paisaje.</strong> Isologotipo superpuesto a la imagen.</p>
+    <p><strong>Footer dividido en 3:</strong> [Nombre agencia y Provincia, país.][Propietario web, mail y teléfono.][Certificado oficial del gobierno y link para denunciar agencia]</p>
+    <p><strong>Segundo footer</strong> de copyright - año - Nombre agencia - Provincia y país - Programador web.</p>
+    </div>
+    );
+  }
+  
+  export default Home;
