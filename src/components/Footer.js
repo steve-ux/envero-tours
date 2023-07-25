22a2e54f86fa05425882b0c53d11d3bfb42165a1
steve-ux <exe.universal@gmail.com>
2023-07-25 20:57:32 -0300

    se cambió el PDF del permiso

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,13 @@ function Footer() {
         </div>
         <div data-aos="fade-down" className="div2">
         <h4>This Is Mendoza S.A.</h4>
-        <p>Permiso precario número: PV-2022-136383912</p>
+        <p>Certificado permiso provisorio: N° 18541</p>
         <p>Mail: <a href="mailto:nicolasromero88@gmail.com">nicolasromero88@gmail.com</a></p>
         <p>Cel: <a href="tel:+54 9 261 341 3569">+54 9 261 341 3569</a></p>
         </div>
         <div data-aos="fade-right" className="div3">
         <img src={LogoMinisterio} alt="Logo Ministerio de Turismo y Deportes" width="100"/>
-        <h4>Certificado permiso precario, <a href="https://raw.githubusercontent.com/steve-ux/envero-tours/e2d516d18fee2ebe8d12b1166e708e38dcf4083f/src/assets/PV-2022-136383912-APN-DRAV%2523MTYD.pdf" target="_blank" rel="noreferrer">aquí.</a></h4>
+        <h4>Certificado permiso provisorio, <a href="https://raw.githubusercontent.com/steve-ux/envero-tours/e2d516d18fee2ebe8d12b1166e708e38dcf4083f/src/assets/PV-2022-136383912-APN-DRAV%2523MTYD.pdf" target="_blank" rel="noreferrer">aquí.</a></h4>
         <p>Para denunciar una agencia, <a href="https://www.argentina.gob.ar/servicio/presentar-una-denuncia-contra-una-agencia-de-viajes" target="_blank" rel="noreferrer">hacé clic acá.</a></p>
         </div>  
     </div>
